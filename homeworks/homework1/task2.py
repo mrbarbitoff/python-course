d4ed957257a0a15e0f46270aca7b9f551732ab10
mrbarbitoff <barbitoff@bk.ru>
2015-09-26 14:23:37 +0300

    Corrected path

---
+++ homeworks/homework1/task2.py
@@ -0,0 +1,8 @@
+#Task 2
+inp = input()
+prepr = inp.split()
+numbers = []
+for number in prepr:
+    numbers.append(int(number))
+print(sum(numbers)/len(numbers)) 
+#Alternatively, could use numpy.mean() that does not work in Stepic
