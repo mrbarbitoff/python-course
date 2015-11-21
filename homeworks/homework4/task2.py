4ac0a27e86030dfc96f542e11bc8c0cc6b6cc273
Yury Barbitoff <barbitoff@bk.ru>
2015-11-21 17:12:08 +0300

    Saturdaaaaaaaaaaaaaaaaay

---
+++ homeworks/homework4/task2.py
@@ -0,0 +1,24 @@
+# Task 2
+import math
+
+
+nouns = set()
+adjectives = set()
+verbs = set()
+
+with open('dict.txt', 'r') as fh:
+    for line in fh:
+        line = line.rstrip()
+        if line.endswith('ka'):
+            nouns.add(line)
+        elif line.endswith('yo'):
+            adjectives.add(line)
+        else:
+            verbs.add(line)
+
+adjcomb = 0
+wheretostop = 7 if len(adjectives) > 7 else len(adjectives)
+for i in range(1, wheretostop + 1):
+    adjcomb += math.factorial(len(adjectives))/math.factorial(len(adjectives) - i)       
+numsentences = int(len(nouns)*len(verbs)*adjcomb)
+print(numsentences)
