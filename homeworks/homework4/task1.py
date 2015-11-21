e04f9b829fa49c183211a30bd8ced7b29a1726d3
Yury Barbitoff <barbitoff@bk.ru>
2015-11-21 17:10:38 +0300

    HW4 pushed right and HW5 sync

--- homeworks/homework4/task1.py
+++ homeworks/homework4/task1.py
@@ -1,5 +1,22 @@
-import re
-import sys
+# This variant is f***ing memory-efficient, though not fancy
 
-text = sys.stdin.read()
-print(len(re.findall('[Yy]ou', text)))
+with open('yazkora.txt', 'r') as fh:
+    with open('answer.txt', 'w') as ofile:
+        sentence = ''
+        adjectives = ''
+        for line in fh:
+            line = line.rstrip() + ' '
+            dots = [x for x, s in enumerate(line) if s == '.']
+            dots.append(len(line))
+            previous_offset = 0
+            for offset in dots:
+                sentence += line[previous_offset:offset]
+                if offset != len(line):
+                    content = sentence.split()
+                    for word in content:
+                        if word.endswith('yo'):
+                            adjectives += word + ' '
+                    ofile.write(adjectives + '\n')
+                    sentence = ''
+                    adjectives = ''
+                    previous_offset = offset + 1
