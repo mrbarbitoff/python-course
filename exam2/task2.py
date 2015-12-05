b130a48ea3ed73ad8748a8913626ed102cc99303
Yury Barbitoff <barbitoff@bk.ru>
2015-12-05 13:22:44 +0300

    final sync e2

--- exam2/task2.py
+++ exam2/task2.py
@@ -1,4 +1,5 @@
 # Task 2
+# Order of symdols matters a lot, huh
 import re
 import requests
 
@@ -11,7 +12,7 @@ with open('links.txt', 'r') as ifile:
 mailinglst = set()
 for lnk in addresses:
     data = requests.get(lnk)
-    mails = re.findall('[\w\.]+@[\w\.]+', data.text)
+    mails = re.findall('[.\w]+@[.\w]+', data.text)  # re.findall('[\w\.]+@[\w\.]+', data.text) yields 10 mails, as well as \.\w and \w.
     for mail in mails:
         if len(re.findall('^\.|\.$|\.@|@\.', mail)) == 0:
             mailinglst.add(mail)
