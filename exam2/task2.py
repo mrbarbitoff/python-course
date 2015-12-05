6a20574ed35afb88e197eb15748510ab73809b5b
Yury Barbitoff <barbitoff@bk.ru>
2015-12-05 12:35:49 +0300

    e2t2

---
+++ exam2/task2.py
@@ -0,0 +1,19 @@
+# Task 2
+import re
+import requests
+
+addresses = []
+with open('links.txt', 'r') as ifile:
+    for line in ifile:
+        addresses.append(line.strip())
+
+
+mailinglst = set()
+for lnk in addresses:
+    data = requests.get(lnk)
+    mails = re.findall('[\w.]+@[\w.]+', data.text)
+    for mail in mails:
+        if len(re.findall('^\.|\.$|\.@|@\.', mail)) == 0:
+            mailinglst.add(mail)
+
+print("\n".join(mailinglst))
